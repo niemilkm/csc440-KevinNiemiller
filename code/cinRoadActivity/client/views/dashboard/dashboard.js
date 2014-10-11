b3d0729e8315d60f90df5f62ebad8e98b7bff233
Kevin NIemiller <niemilkm@gmail.com>
2014-10-11 11:13:50 -0400

    before adding new nom for xml

--- code/cinRoadActivity/client/views/dashboard/dashboard.js
+++ code/cinRoadActivity/client/views/dashboard/dashboard.js
@@ -23,49 +23,58 @@ Template.dashboard.events =
   {
     'click button.importData': function()
     {
-      Session.set("filter", "all");
-      if (window.XMLHttpRequest)
-      {
-        xhttp=new XMLHttpRequest();
-      }
-      else // code for IE5 and IE6
-      {
-        xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
-      xhttp.open("GET","http://www.buckeyetraffic.org/services/RoadActivity.aspx",false);
-      //xhttp.open("GET","http://localhost:3000/roadActivity_29SEP14.xml",false);
-      //xhttp.open("GET","http://cinRoadActivity.meteor.com/roadActivityPartial.xml",false);
-      //xhttp.open("GET","http://ec2-54-68-187-59.us-west-2.compute.amazonaws.com/roadActivity_29SEP14.xml",false);
-      xhttp.setRequestHeader('User-Agent','XMLHTTP/1.0');
-      //xhttp.setRequestHeader('Content-Type', 'application/xml');
-      xhttp.send();
-      xmlDoc = xhttp.responseXML;
-      var Id=""; var Category=""; var Status=""; var Direction=""; var Road=""; var CountyCode=""; var DistrictNumber=-1; var Latitude=-1.1; var Longitude=-1.1; var ActivityStartDateTime=""; var ActivityEndDateTime=""; var ActivityCreationDateTime=""; var ActivityLastModifiedDateTime=""; var StartMile=-1; var StartMileDescription = ""; var EndMile=-1; var EndMileDescription=""; var Description=""; var DetourDescription="";
-      var dataKeywords = ["Id", "Category", "Status", "Direction", "Road", "CountyCode", "DistrictNumber", "Latitude", "Longitude", "ActivityStartDateTime", "ActivityEndDateTime", "ActivityCreationDateTime", "ActivityLastModifiedDateTime", "StartMile", "StartMileDescription", "EndMile", "EndMileDescription", "Description", "DetourDescription"];
-      var dataValuesArray = [Id, Category, Status, Direction, Road, CountyCode, DistrictNumber, Latitude, Longitude, ActivityStartDateTime, ActivityEndDateTime, ActivityCreationDateTime, ActivityLastModifiedDateTime, StartMile, StartMileDescription, EndMile, EndMileDescription, Description, DetourDescription];
-      var dataValues = [Id, Category, Status, Direction, Road, CountyCode, DistrictNumber, Latitude, Longitude, ActivityStartDateTime, ActivityEndDateTime, ActivityCreationDateTime, ActivityLastModifiedDateTime, StartMile, StartMileDescription, EndMile, EndMileDescription, Description, DetourDescription];
-      var roadActivity= xmlDoc.getElementsByTagName("RoadActivity");
-      for (var j = 0; j < 18; j++) //still need to add contact info
-      {
-        dataValuesArray[j] = xmlDoc.getElementsByTagName(dataKeywords[j]);
-      }
-      for ( var i = 0; i < roadActivity.length; i++ ) // Each Road Activity
-      {
-        dataValues[0] = dataValuesArray[0][i].childNodes[0].nodeValue;
-        var count = RoadActivity.find({_id: dataValues[0]}).count();
-        if (count == 0)
-        {
-          for (var k = 1; k < 18; k++) // Each item (Id, Category, Status, etc.) of each Road Activity
-          {
-            if (dataValuesArray[k][i].childNodes[0] != undefined)
-            {
-              dataValues[k] = dataValuesArray[k][i].childNodes[0].nodeValue;
-            }
-          }
-          Meteor.call("insert_data", dataValues);
-        }
-      }
-      console.log("dashboard importData complete");
+      // Session.set("filter", "all");
+      // if (window.XMLHttpRequest)
+      // {
+      //   xhttp=new XMLHttpRequest();
+      // }
+      // else // code for IE5 and IE6
+      // {
+      //   xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+      // }
+      // xhttp.open("GET","http://www.buckeyetraffic.org/services/RoadActivity.aspx",false);
+      // //xhttp.open("GET","http://localhost:3000/roadActivity_29SEP14.xml",false);
+      // //xhttp.open("GET","http://cinRoadActivity.meteor.com/roadActivityPartial.xml",false);
+      // //xhttp.open("GET","http://ec2-54-68-187-59.us-west-2.compute.amazonaws.com/roadActivity_29SEP14.xml",false);
+      // xhttp.setRequestHeader('User-Agent','XMLHTTP/1.0');
+      // //xhttp.setRequestHeader('Content-Type', 'application/xml');
+      // xhttp.send();
+      // xmlDoc = xhttp.responseXML;
+      // var Id=""; var Category=""; var Status=""; var Direction=""; var Road=""; var CountyCode=""; var DistrictNumber=-1; var Latitude=-1.1; var Longitude=-1.1; var ActivityStartDateTime=""; var ActivityEndDateTime=""; var ActivityCreationDateTime=""; var ActivityLastModifiedDateTime=""; var StartMile=-1; var StartMileDescription = ""; var EndMile=-1; var EndMileDescription=""; var Description=""; var DetourDescription="";
+      // var dataKeywords = ["Id", "Category", "Status", "Direction", "Road", "CountyCode", "DistrictNumber", "Latitude", "Longitude", "ActivityStartDateTime", "ActivityEndDateTime", "ActivityCreationDateTime", "ActivityLastModifiedDateTime", "StartMile", "StartMileDescription", "EndMile", "EndMileDescription", "Description", "DetourDescription"];
+      // var dataValuesArray = [Id, Category, Status, Direction, Road, CountyCode, DistrictNumber, Latitude, Longitude, ActivityStartDateTime, ActivityEndDateTime, ActivityCreationDateTime, ActivityLastModifiedDateTime, StartMile, StartMileDescription, EndMile, EndMileDescription, Description, DetourDescription];
+      // var dataValues = [Id, Category, Status, Direction, Road, CountyCode, DistrictNumber, Latitude, Longitude, ActivityStartDateTime, ActivityEndDateTime, ActivityCreationDateTime, ActivityLastModifiedDateTime, StartMile, StartMileDescription, EndMile, EndMileDescription, Description, DetourDescription];
+      // var roadActivity= xmlDoc.getElementsByTagName("RoadActivity");
+      // for (var j = 0; j < 18; j++) //still need to add contact info
+      // {
+      //   dataValuesArray[j] = xmlDoc.getElementsByTagName(dataKeywords[j]);
+      // }
+      // for ( var i = 0; i < roadActivity.length; i++ ) // Each Road Activity
+      // {
+      //   dataValues[0] = dataValuesArray[0][i].childNodes[0].nodeValue;
+      //   var count = RoadActivity.find({_id: dataValues[0]}).count();
+      //   if (count == 0)
+      //   {
+      //     for (var k = 1; k < 18; k++) // Each item (Id, Category, Status, etc.) of each Road Activity
+      //     {
+      //       if (dataValuesArray[k][i].childNodes[0] != undefined)
+      //       {
+      //         dataValues[k] = dataValuesArray[k][i].childNodes[0].nodeValue;
+      //       }
+      //     }
+      //     Meteor.call("insert_data", dataValues);
+      //   }
+      // }
+
+      //Where new code starts
+      Meteor.call("importData", function(error, result){
+        if (error)
+          console.log("error in importData");
+        else
+          console.log("importData complete");
+      });
+      //where new code ends
+      console.log("dashboardClick importData complete");
     },
 
     'click button.filterToday': function()
--- code/cinRoadActivity/server/views/importData.js
+++ code/cinRoadActivity/server/views/importData.js
@@ -3,160 +3,42 @@ Meteor.methods({
 
 	importData: function() {
 		// Create the return object
-			console.log("import data by user start");
-			// var DOMParser = require('xmldom').DOMParser;
-			// var parser = new DOMParser();
-			//xmlHttp = http.XMLHttpRequest()
+		console.log("import data by user start");
 			
+			// var xhttp = new XMLHttpRequest();
+			// xhttp.open("GET","http://www.buckeyetraffic.org/services/RoadActivity.aspx",false);
+   //    xhttp.setRequestHeader('User-Agent','XMLHTTP/1.0');
+   //    //xhttp.setRequestHeader('Content-Type', 'application/xml');
+   //    xhttp.send();
+   //    xmlDoc = xhttp.responseXML;
+   //    console.log(xmlDoc);
 
-			// var data = xmlDoc.getElementsByTagName("CountyCode")[0].childNodes[0].nodeValue; //[0].childNodes[0].nodeValue;  
-   //    console.log(data);
+  //  	$req = new HTTP_Request2($_GET['http://www.buckeyetraffic.org/services/RoadActivity.aspx']);
+		// $response = $req.send();
+		// // Output the content-type header and use the content-type of the original file
+		// header("Content-type: " . $response.getHeader("Content-type"));
+		// // And provide the file body
+		// console.log($response.getBody());
 
+		// $_get( "http://www.buckeyetraffic.org/services/RoadActivity.aspx", function( data ) {
+		//   console.log( "Data Loaded: " + data );
+		// });
 
+		HTTP.call("POST", "http://www.buckeyetraffic.org/services/RoadActivity.aspx", function(error, result) {
+			if (!error) {
+				//console.log(result);
+				var status = result.getElementsByTagName("Status");
+				console.log(status);
+			}
+			else
+				console.log("error");
+		});
 
+		var doc = libxml.parseXmlString(result);
+		console.log(doc);
 
+    return true;
 
-			xmlDoc = Meteor.http.call("GET", "http://www.buckeyetraffic.org/services/RoadActivity.aspx");
-			console.log(xmlDoc);
-			// if (window.DOMParser)
-		 //  {
-		 //    parser=new DOMParser();
-		 //    xmlDoc=parser.parseFromString(xml,"text/xml");
-		 //  }
-			// else // Internet Explorer
-		 //  {
-		 //    xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-		 //    xmlDoc.async=false;
-		 //    xmlDoc.loadXML(txt);
-		 //  }
-
-		 //  var data = xmlDoc.getElementsByTagName("Status")[0].childNodes[0].nodeValue;	
-		 //  console.log(data);
-
-
-
-      // xhttp=new XMLHttpRequest();
-      // xhttp.open("GET","http://localhost:3000/roadActivityPartial.xml",false);
-      // xhttp.send();
-      // xml = xmlDoc.responseXML;
-
-      // console.log(xml);
-
-      // var data = xmlDoc.getElementsByTagName("CountyCode")[0].childNodes[0].nodeValue; //[0].childNodes[0].nodeValue;  
-      // console.log(data);
-
-
-
-
-
-// 		 var data = xmlDoc.getElementsByTagName("CountyCode")[0].childNodes[0].nodeValue;
-// 		 console.log(data);
-
-// //working start
-// 			var builder = new xml2js.Builder();
-// 			var json = builder.buildObject(xml);
-			
-// 			var extractedData = "";
-// 			var parser = new xml2js.Parser();
-// 			parser.parseString(json, function(err,result){
-
-// 			});
-
-
-
-			// var id = "a40f6cca7d444199a40bd065e41f66b";
-			// Meteor.call("insert_xml", json, id);
-
-// workign end
-
-
-
-//console.log("xml:====> " + xml);
-//console.log("JSON ======> " + JSON.stringify(json));
-//Extract the value from the data element
-			  //extractedData = result['data'];
-			  //console.log("result ==> " + result);
-			 //  var id = "3b40f6cca7d444199a40bd065e41f66b";
-				// Meteor.call("insert_xml", result, id);
-			
-			 //console.log(xmlDoc);
-			 //var xml = .parseXML(xmlDoc);
-			 //xml = xmlHttp.responseXML.documentElement;
-			 //console.log(xml);
-
-			 //var json = xml2js.parseXML(xml);
-			//  var XML2JS = Meteor.require('xml2js');
-			//  var xml2js = new XML2JS();
-			 //var json = xml2js.parseString(xml);
-
-			
-			//console.log(json);
-			//console.log("==> " + JSON.stringify(json));
-			// console.log("_________");
-			// var xml = parser.parseFromString(xmlHttp, "text/xml");
-			
-			//var obj = xml2js.parseFromString(xml);
-			//console.log(obj);
-
-			//var xml=loadXMLDoc(xmlDoc);
-
-			//var x2js = new X2JS();
-
-			
-			//xmlHttp.send();
-			// xmlHttp = new window.XMLHttpRequest();
-   //    xmlHttp.open("GET", "roadActivityPartial.xml", false);
-   //    xmlHttp.send(null);
-      //xml = xmlHttp.responseXML.documentElement;
-      //console.log(xml);
-
-			//debugger;
-
-			// var obj = {};
-
-			// if (xml.nodeType == 1) { // element
-			// 	// do attributes
-			// 	console.log("do attributes");
-			// 	if (xml.attributes.length > 0) {
-			// 	obj["@attributes"] = {};
-			// 		for (var j = 0; j < xml.attributes.length; j++) {
-			// 			var attribute = xml.attributes.item(j);
-			// 			obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			// 		}
-			// 	}
-			// } else if (xml.nodeType == 3) { // text
-			// 	obj = xml.nodeValue;
-			// }
-
-			// console.log("importData server - before doing children");
-			// //do children
-			// if (xml.hasChildNodes()) {
-			// 	for(var i = 0; i < xml.childNodes.length; i++) {
-			// 		var item = xml.childNodes.item(i);
-			// 		var nodeName = item.nodeName;
-			// 		if (typeof(obj[nodeName]) == "undefined") {
-			// 			obj[nodeName] = xmlToJson(item);
-			// 		} else {
-			// 			if (typeof(obj[nodeName].push) == "undefined") {
-			// 				var old = obj[nodeName];
-			// 				obj[nodeName] = [];
-			// 				obj[nodeName].push(old);
-			// 			}
-			// 			obj[nodeName].push(xmlToJson(item));
-			// 		}
-			// 	}
-			// }
-			// console.log("import data by user end");
-			// obj = JSON.parse(obj);
-			// Fiber(function () {
-
-		 //    _.each(data, function (document) {
-		 //      RoadActivity.insert(document);
-		 //    });
-
-  	// 	}).run();
-			// console.log(obj);
-			// return obj;
 		}
 
 
