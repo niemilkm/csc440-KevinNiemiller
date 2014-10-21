ca84bd56221c6dff5c3b2638273e716949b7b9c7
Kevin NIemiller <niemilkm@gmail.com>
2014-10-20 20:52:49 -0400

    update deployment file

--- code/cinRoadActivity/meteoric.sh
+++ code/cinRoadActivity/meteoric.sh
@@ -125,7 +125,7 @@ sudo npm install -g underscore;
 sudo npm install -g source-map-support;
 sudo npm install -g semver;
 
-export ROOT_URL="http://ec2-54-68-187-59.us-west-2.compute.amazonaws.com";
+export ROOT_URL="http://ec2-54-68-136-133.us-west-2.compute.amazonaws.com/";
 export BIND_IP="0.0.0.0";
 export PORT="80";
 export MONGO_URL="mongodb://localhost:27017/cinRoadActivity"; 
