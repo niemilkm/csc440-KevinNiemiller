05425bea99e2c3d00c55d141dafc6878bc65c9b4
Kevin NIemiller <niemilkm@gmail.com>
2014-10-22 21:03:28 -0400

    deployment

--- code/cinRoadActivity/meteoric.sh
+++ code/cinRoadActivity/meteoric.sh
@@ -117,19 +117,6 @@ fi;
 DEPLOY="$DEPLOY
 echo Starting forever;
 sudo -E forever restart bundle/main.js || sudo -E forever start bundle/main.js;
-cd ~;
-sudo npm install -g fibers;
-sudo npm install -g underscore;
-sudo npm install -g source-map-support;
-sudo npm install -g semver;
-
-export ROOT_URL="http://ec2-54-68-136-133.us-west-2.compute.amazonaws.com/";
-export BIND_IP="0.0.0.0";
-export PORT="80";
-export MONGO_URL="mongodb://localhost:27017/cinRoadActivity"; 
-cd /home/meteor/cinRoadActivity/code;
-sudo -E forever stopall;
-sudo -E forever start bundle/main.js;
 "
 
 START=<<EOF
