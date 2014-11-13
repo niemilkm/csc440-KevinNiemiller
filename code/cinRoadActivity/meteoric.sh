193a544551efd2025ade04030043b38697024bf3
Kevin NIemiller <niemilkm@gmail.com>
2014-11-12 22:23:20 -0500

    Finally got my instance running again

--- code/cinRoadActivity/meteoric.sh
+++ code/cinRoadActivity/meteoric.sh
@@ -61,7 +61,7 @@ sudo apt-get install software-properties-common;
 sudo add-apt-repository ppa:chris-lea/node.js;
 sudo apt-get -qq update;
 sudo apt-get install git mongodb;
-sudo apt-get install nodejs npm;
+sudo apt-get install nodejs;
 node --version;
 sudo npm install -g forever;
 curl https://install.meteor.com | /bin/sh;
